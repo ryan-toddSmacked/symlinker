f9b941b411ccd6e88cae099444dccd6c01f1a63f
ryan-toddSmacked <94807949+ryan-toddSmacked@users.noreply.github.com>
2023-10-06 16:41:38 -0600

    updated, to follow functionality of rsync

--- symlink.py
+++ symlink.py
@@ -1,107 +1,130 @@
+#!/usr/bin/env python3
 
-import argparse as ap
 import os
+import argparse
 import glob
-import errno
+import fnmatch
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Create symbolic links")
 
-    parser = ap.ArgumentParser(prog="symlink", description='Create symlinks of files to a specified directory.')
-    parser.add_argument('-b', metavar="--base-dir", type=str, nargs=1, required=True, help='The base directory to search for files.')
-    parser.add_argument('-t', metavar="--token", type=str, nargs='+', required=True, help='A glob token to search for in the filenames.')
-    parser.add_argument('-d', metavar="--dest-dir", type=str, nargs=1, required=True, help='The destination directory to create the symlinks in.')
-    parser.add_argument('--recursive', action="store_true", help='Whether to search subdirectories.')
-    parser.add_argument('--prune', action="store_true", help='Whether to prune the destination directory of intermediate sub-directories from the base directory, just keeping the files.')
-    parser.add_argument('--verbose', action="store_true", help='Whether to print verbose output.')
-
+    parser.add_argument("source", help="Source file or directory")
+    parser.add_argument("destination", help="Destination file or directory")
+    parser.add_argument("-r", "--recursive", action="store_true", help="Check files recursively")
+    parser.add_argument("-i", "--include", nargs="*", type=str, help="Include only files or directories matching this pattern")
+    parser.add_argument("-e", "--exclude", nargs="*", type=str, help="Exclude files or directories matching this pattern")
+    parser.add_argument("-c", "--create", action="store_true", help="Create destination directory if it does not exist")
+    parser.add_argument("-f", "--force", action="store_true", help="Force overwrite of existing files")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Print verbose output")
+    parser.add_argument("-p", "--prune", action="store_true", help="Prune empty directories")
     args = parser.parse_args()
 
-    # Get the base directory
-    base_dir = args.b[0]
-
-    # Get the glob tokens
-    glob_tokens = args.t
-
-    # Get the destination directory
-    dest_dir = args.d[0]
-
-    # Get the recursive flag
-    recursive = args.recursive
-
-    # Get the prune flag
-    prune = args.prune
-
-    # Get the verbose flag
-    verbose = args.verbose
-
-    if (verbose):
-        print("Base directory: {}".format(base_dir))
-        print("Glob tokens: {}".format(glob_tokens))
-        print("Destination directory: {}".format(dest_dir))
-        print("Recursive: {}".format(recursive))
-        print("Prune: {}".format(prune))
-
-    # Get the files
+    
+    # Get a list of all the files
     files = []
 
-    for token in glob_tokens:
-        if (recursive):
-            files.extend(glob.glob(os.path.join(base_dir, "**", token), recursive=True))
-        else:
-            files.extend(glob.glob(os.path.join(base_dir, token)))
-        
-        if (verbose):
-            print("Found {} files matching token: {}".format(len(files), token))
-            for f in files:
-                print(f)
-        
-        # sift out the unique files
-        files = list(set(files))
+    if args.recursive:
+        # use glob to get all the files
+        files = glob.glob(args.source + "/**", recursive=True)
+    else:
+        files = glob.glob(args.source + "/*")
 
-    # Run through the files and create symlinks, attempting to create directories as needed
-    for f in files:
-        # Get the filename
-        filename = os.path.basename(f)
+    # Remove source path from files list
+    files = [x.replace(args.source, '') for x in files]
 
-        # Get the directory
-        directory = os.path.dirname(f)
+    excluded_files = []
+    if args.exclude:
+        for pattern in args.exclude:
+            excluded_files.extend(fnmatch.filter(files, pattern))
 
-        # Get the destination directory
-        if (prune):
-            dest_directory = dest_dir
-        else:
-            dest_directory = os.path.join(dest_dir, os.path.relpath(directory, base_dir))
+    # loop through the include patterns and remove and excluded files that match the pattern
+    if args.include:
+        for pattern in args.include:
+            # find all excluded files that match the pattern
+            matches = fnmatch.filter(excluded_files, pattern)
+            # remove the matches from the excluded files list
+            for match in matches:
+                excluded_files.remove(match)
 
-        # Create the destination directory if it doesn't exist
-        if not os.path.exists(dest_directory):
-            try:
-                os.makedirs(dest_directory)
-            except OSError as e:
-                if e.errno != errno.EEXIST:
-                    # Display the error, and continue
-                    print("Error creating directory: {}".format(dest_directory))
-                    print(e)
-                    continue
 
-        # Create the symlink
-        symlink = os.path.join(dest_directory, filename)
-        if (verbose):
-            print("Creating symlink: {} -> {}".format(symlink, f))
+    # remove the excluded files from the files list
+    for file in excluded_files:
+        files.remove(file)
+
+    # if prune is set, remove empty directories from files list
+    if args.prune:
+        for file in files:
+            if os.path.isdir(file):
+                # This is a directory, check if it is empty
+                if not os.listdir(file):
+                    # Directory is empty, remove it from list
+                    files.remove(file)
+
+    # Create the symbolic links, Replace the source string with the target string
+    # Append the source string to the files list to create the src_files list
+    # Append the destination string to the files list to create the dest_files list
+    src_files = [args.source + x for x in files]
+    dest_files = [args.destination + x for x in files]
+
+    idx = 0
+    while (idx < len(files)):
+        file = src_files[idx]
+        destination = dest_files[idx]
+    
+        # put the symlink operation in a try block
+        # os.symlink can throw the following exceptions: FileExistsError, FileNotFoundError, PermissionError
         try:
-            os.symlink(f, symlink)
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                # Display the error, and continue
-                print("Error creating symlink: {} -> {}".format(symlink, f))
-                print(e)
-                continue
+            os.symlink(file, destination)
+            if args.verbose:
+                print(f'Created symlink: {destination} -> {file}')
+        except FileExistsError:
+            if args.verbose and not args.force:
+                print(f'Symlink already exists: {destination} -> {file}')
             else:
-                # The symlink already exists, so continue
-                continue
+                # Delete the destination file and try to create the symlink again
+                try:
+                    os.remove(destination)
+                    # decrement the index so we can try to create the symlink again
+                    idx = idx - 1
+                except:
+                    print(f'Could not delete file: {destination}')
+                pass
+            pass
+        except FileNotFoundError:
+            if args.verbose and not args.create:
+                print(f'File not found: {file}')
+            else:
+                # Attempt to create the output directory
+                try:
+                    os.makedirs(os.path.dirname(destination), exist_ok=True)
+                    # decrement the index so we can try to create the symlink again
+                    idx = idx - 1
+                except:
+                    print(f'Could not create directory: {os.path.dirname(destination)}')
+                pass
+            pass
+        except PermissionError:
+            if args.verbose:
+                print(f'Permission denied: {file}')
+            pass
+        except OSError:
+            if args.verbose:
+                print(f'OS Error: {file}')
+            pass
+        except:
+            if args.verbose:
+                print(f'Unknown Error: {file}')
+            pass
+        idx = idx + 1
 
     pass
 
-
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
